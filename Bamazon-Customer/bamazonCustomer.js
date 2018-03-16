2fd07389f89a2652955ebe1b64c652fb1fbe7772
Devon Williams <dvnwllms009@gmail.com>
2018-03-15 22:47:19 -0400

    removedPassword

--- Bamazon-Customer/bamazonCustomer.js
+++ Bamazon-Customer/bamazonCustomer.js
@@ -38,7 +38,7 @@ function showData() {
                 type: "input",
                 message: "How many do you want?"
             }
-            
+
         ]).then(function(checkOut) {
 
             var item = checkOut.idselect;
